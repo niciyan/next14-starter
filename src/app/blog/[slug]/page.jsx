337cce2908cdca53bf38ec03301cf532373a2ab6
niciyan <nishiyamasu@gmail.com>
2024-08-12 13:18:14 +0900

    navbar styling done.

---
+++ src/app/blog/[slug]/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SinglePostPage = () => {
+  return <div>single post page</div>;
+};
+
+export default SinglePostPage;
---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const NotFount = () => {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <Link href="/">return home.</Link>
+    </div>
+  );
+};
+
+export default NotFount;
