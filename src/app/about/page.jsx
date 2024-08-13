e6567e73b6ecf65cb3767695fb05b81a77dd5748
niciyan <nishiyamasu@gmail.com>
2024-08-14 00:46:19 +0900

    finished 1:26:08 contact page.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,9 +2,35 @@ import Image from "next/image";
 import styles from "./about.module.css";
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
+          pariatur explicabo quis possimus iure illo obcaecati tempore
+          accusantium animi voluptatibus. Ad dignissimos adipisci molestias
+          iusto, odio quas officiis doloremque magnam.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image src="/about.png" alt="" fill />
+        <Image src="/about.png" alt="" fill className={styles.img} />
       </div>
     </div>
   );
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>lamadev</div>
+      <div className={styles.text}>
+        Lama creative thoughts agency, All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
